9ae7a6c23bd9033a79c1782cea84472cf1ebbc2b
Tomáš Dračka <tomidracka@gmail.com>
2023-01-04 12:56:31 +0100

    Main extends Application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args){
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Main.class.getResource("gui/View/MainWindow.fxml"));
+        primaryStage.setTitle("Application");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
---
+++ src/gui/Controller/MainController.java
@@ -0,0 +1,18 @@
+package gui.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private Button btn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
