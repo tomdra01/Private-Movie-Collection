e78ab281639881acb17aab02ba8a101fbc30c68b
Tomáš Dračka <tomidracka@gmail.com>
2023-01-04 11:29:04 +0100

    Base start

---
+++ src/be/Category.java
@@ -0,0 +1,13 @@
+package be;
+
+public class Category {
+
+    private int id;
+
+    private String name;
+
+    public Category(){
+
+    }
+
+}
---
+++ src/be/Movie.java
@@ -0,0 +1,21 @@
+package be;
+
+import java.io.File;
+
+public class Movie {
+
+    private int id;
+
+    private String name;
+
+    private double rating;
+
+    private File fileLink;
+
+    private String lastView;
+
+    public Movie(){
+
+    }
+
+}
