b8baf25fb1cb9007fb0913a843bcbcf737ed2829
FreddyFlag <31618798+FreddyFlag@users.noreply.github.com>
2023-01-05 11:24:25 +0100

    DbConnector

--- src/dal/DatabaseConnector.java
+++ src/dal/DatabaseConnector.java
@@ -1,4 +1,34 @@
 package dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+
+    private SQLServerDataSource dataSource;
+
+    public DatabaseConnector(){
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("PMC2022");
+        dataSource.setUser("CSe22B_11");
+        dataSource.setPassword("CSe22B_11");
+        dataSource.setPortNumber(1433);
+        dataSource.setTrustServerCertificate(true);
+    }
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnector dataBaseConnection = new DatabaseConnector();
+        Connection connection = dataBaseConnection.getConnection();
+
+        System.out.println("Try to open " + !connection.isClosed());
+
+        connection.close();
+    }
 }
